f32bd9307c2f50757a3332d61a5b35f69138ea7b
nagobaa <fej@hotmail.fi>
2025-08-04 20:18:39 +0300

    fdsa

--- Assets/Scripts/Company.cs
+++ Assets/Scripts/Company.cs
@@ -7,8 +7,10 @@ public class Company : MonoBehaviour
 {
     //hairdresser
     //method: CutHair
+    //hairGrowth = 
     //men_price = addedValueTax * men_price
     //women_price = addedValueTax * women_price
+    //
     //float 
 
     //hotel
@@ -16,8 +18,11 @@ public class Company : MonoBehaviour
     //method: CustomerUnhappy
     //parameter
     //string levelOfHotel = "5 star"
+    //float hotelPrice = 100f;
 
     //general
+    //if money -> employ more employees
+    //if money -> expanda business
     //if no money -> fire emplyees
     //if no money -> shut down business
 }
