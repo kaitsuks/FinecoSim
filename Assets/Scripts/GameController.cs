3e66ef5e9c7fc491469c4f0d62065453e46c10c7
nagobaa <fej@hotmail.fi>
2025-09-19 23:30:49 +0300

    20250919 Updates to panel 2 of 3
    20250919 Updates to panel 2 of 3

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,41 +9,52 @@ public class GameController : MonoBehaviour
     public GameObject graphPanel;
 
     [Header("References")]
-    public Button simulationButton; // the button in SimulationButtonPanel
+    public Button simulationButton;
 
     private bool startMenuPassed = false;
     private bool simulationStarted = false;
 
     void Start()
     {
-        // Show only the StartMenu in the beginning
+        // Showing only the StartMenu in the beginning
         startMenuPanel.SetActive(true);
         simulationButtonPanel.SetActive(false);
         graphPanel.SetActive(false);
 
-        // Connecting the function of the button
+        // connection the press of button to "StartSimulation()"
         simulationButton.onClick.AddListener(StartSimulation);
     }
 
     void Update()
     {
-        if (!startMenuPassed)
+        if (!startMenuPassed) // start menu
         {
-            if (Input.GetKeyDown(KeyCode.Return)) // Enter
+            if (Input.GetKeyDown(KeyCode.Return))
             {
+                Debug.Log("1. the start menu has been passed with ENTER");
                 startMenuPassed = true;
                 startMenuPanel.SetActive(false);
-                simulationButtonPanel.SetActive(true); // show the button
+                simulationButtonPanel.SetActive(true); // showing the button
             }
             else if (Input.GetKeyDown(KeyCode.Escape))
             {
+                Debug.Log("2. One has cancelled from the start menu with ESQ");
                 Application.Quit();
             }
         }
-        else if (simulationStarted)
+        else if (!simulationStarted) // the panel with the simulation button
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
+                Debug.Log("4. One has cancelled in the panel with the simulation button with ESQ");
+                Application.Quit();
+            }
+        }
+        else if (simulationStarted) // the simulation is run
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("6. One has cancelled under simulation with ESC");
                 Application.Quit();
             }
         }
@@ -51,8 +62,10 @@ public class GameController : MonoBehaviour
 
     private void StartSimulation()
     {
+        Debug.Log("3. The simulation button has been pressed");
         simulationStarted = true;
-        simulationButtonPanel.SetActive(false);
-        graphPanel.SetActive(true); // Showing the graph + text
+        simulationButtonPanel.SetActive(false); // Removing the button
+        graphPanel.SetActive(true); // Showing the graph and texts
+        Debug.Log("5. The simulation has started");
     }
 }
